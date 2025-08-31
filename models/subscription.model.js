07d99035d9d0772c182c3f0170fe81089667333d
Ayush <ayushkumar85385@gmail.com>
2025-08-31 19:29:59 +0530

    added subscription model

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -1,7 +1,20 @@
 import mongoose from "mongoose";
 
 const subscriptionModel = new mongoose.Schema({
-
+  plans: {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    }
+  }
 })
 
 const Subscription = mongoose.model("Subscription", subscriptionModel);
